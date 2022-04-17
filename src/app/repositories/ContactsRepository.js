28af9b177caad466855aff3fcea219857aea207e
Igor Gabarra <igorgabarra@hotmail.com>
2022-04-17 14:01:48 -0300

    feat(contacts): save contact data into db

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,5 +1,7 @@
 const { uuid } = require('uuidv4');
 
+const db = require('../../database');
+
 let contacts = [
   {
     id: uuid(),
@@ -34,20 +36,16 @@ class ContactsRepositories {
     ));
   }
 
-  create({
+  async create({
     name, email, phone, category_id,
   }) {
-    return new Promise((resolve) => {
-      const newContact = {
-        id: uuid(),
-        name,
-        email,
-        phone,
-        category_id,
-      };
-      contacts.push(newContact);
-      resolve(newContact);
-    });
+    const [row] = await db.query(`
+      INSERT INTO contacts(name, email, phone, category_id)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *
+    `, [name, email, phone, category_id]);
+
+    return row;
   }
 
   delete(id) {
