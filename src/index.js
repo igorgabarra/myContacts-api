1dc0c216dd09fae7e9384eb4eaeb9963bb956a10
Igor Gabarra <igorgabarra@hotmail.com>
2022-07-27 00:39:32 -0300

    feat: add middleware for error handler

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const routes = require('./routes');
+require('express-async-errors');
 
 const app = express();
 
 app.use(express.json());
+app.use((error, request, response, next) => {
+  console.log('---- Error handler ----');
+  console.log(error);
+  response.sendStatus(500);
+});
 app.use(routes);
 
 app.listen(3000, () => console.log('Servidor iniciado na porta 3000'));
